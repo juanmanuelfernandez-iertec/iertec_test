04d6e1aa9690b2a55e2c61d3155507123a504389
Juan Manuel Fernández Muñoz <juanmanuel.fernandez@iertec.com>
2021-10-29 08:25:47 +0200

    Fix in cintra ride test execution.

--- cintra_ride_v2/cintra_ride_test_laboratory/do_test_short.bat
+++ cintra_ride_v2/cintra_ride_test_laboratory/do_test_short.bat
@@ -4,6 +4,6 @@ set PATH=C:\ti\ccs1010\ccs\ccs_base\scripting\examples\loadti;%PATH%
 
 cd /d %~dp0
 
-call loadti.bat -c CC2642R1F.ccxml --quiet "cintra_ride_radar.out"
+call loadti.bat -c CC2642R1F.ccxml --quiet "cintra_ride_radar_short.out"
 
 pause
